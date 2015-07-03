90bdf45a85a4ab98341d0fb1771b5aa4efa13319
devSlike <aloha-bit@mail.ru>
2015-07-03 09:42:38 +0300

    2: add button "close" (add button which will close program)

--- GITTest/Form1.cs
+++ GITTest/Form1.cs
@@ -16,5 +16,10 @@ namespace GITTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
